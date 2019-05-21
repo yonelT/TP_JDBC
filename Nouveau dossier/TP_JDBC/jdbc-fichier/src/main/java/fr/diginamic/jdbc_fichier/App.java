abfa31b7f4737a1633f411418d71fbc368c419b2
Yonel TOUSSAY <yonel.toussay@gmail.com>
2019-05-21 18:27:22 +0200

    TP JDBC FICHIER

---
+++ Nouveau dossier/TP_JDBC/jdbc-fichier/src/main/java/fr/diginamic/jdbc_fichier/App.java
@@ -0,0 +1,13 @@
+package fr.diginamic.jdbc_fichier;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
